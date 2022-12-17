857ac9abfe9cdb8c0023afe5f2afbba94b5c5d14
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-12-17 13:50:35 -0800

    Started styling the navigation bar. Did not add functionality to buttons.

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,51 +1,60 @@
 
+import "./Board.css";
+
 function Board () {
+
+    const row1 = Array.from("     ")
+
     return (
-        <div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
+        <div className="row">
+            {row1.map((tile) => <div className="tile">{tile}</div>)}
         </div>
     );
 }
 
 export { Board };
+
+/*
+<ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+*/
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,24 +1,32 @@
+import { useState } from "react";
 import "./NavBar.css";
+import burgerMenu from "./images/burger.png";
+import questionMark from "./images/question-icon.jpg";
+import settings from "./images/settings-gear-icon.webp";
 
 function NavBar() {
+
+    const [titleCentered, setTitleCentered] = useState(true);
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth <= 980) { // innerWidth of 980 or less is for mobile (iPhone)
+            setTitleCentered(false);
+        }
+        else {
+            setTitleCentered(true); // innerWidth of desktop is 980 or more
+        }
+    })
 
     return (
         <nav>
             <div className="more-info">
-                <img href="" alt="More Info"/>
+                <img src={burgerMenu} alt="More Info" className="burger-menu"/>
             </div>
-            <h1 className="wordle-title">WORDLE</h1>
-            <ul className="options">
-                <li>
-                    <img href="" alt="Help"/>
-                </li>
-                <li>
-                    <img href="" alt="Statistics"/>
-                </li>   
-                <li>
-                    <img href="" alt="Settings"/>
-                </li>
-            </ul>
+            <h1 className={titleCentered ? "wordle-title" : "wordle-title left"}>WORDLE</h1>
+            <div className="options">
+                <img src={questionMark} alt="Help" className="question-mark"/>
+                <img src={settings} alt="Settings" className="settings"/>
+            </div>
         </nav>
     );
 }
