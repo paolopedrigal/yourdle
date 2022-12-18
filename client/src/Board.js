d8444458bc8256db65a6c06bb5f151d3e41177b1
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-12-18 03:01:12 -0800

    Provided some logic for inputting guessed letters through keyboard.

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,84 +1,49 @@
-
+import { useEffect } from "react";
 import "./Board.css";
 
 function Board () {
 
-    const row1 = Array.from("     ")
-    const row2 = Array.from("     ")
-    const row3 = Array.from("     ")
-    const row4 = Array.from("     ")
-    const row5 = Array.from("     ")
-    const row6 = Array.from("     ")
+    const numRows = 6;
+    const numTiles = 5;
+    const ids = [...Array(numRows * numTiles).keys()].map((n) => 
+        (n < 26) ? String.fromCharCode(97 + n) : String.fromCharCode(65 + (n - 26))
+    );
+    let idCount = 0;
+    let guessLetter = 0;
+    let guessWordCount = 0
+
+
+    useEffect(() => {
+        document.addEventListener("keydown", (event) => {
+
+            if (event.key === "Backspace" && (guessLetter >= (guessWordCount * numTiles))) { // Deleting guessed letter
+                guessLetter = (guessLetter == (guessWordCount * numTiles)) ? guessLetter : guessLetter - 1;
+                document.getElementById(ids[guessLetter]).innerText = "";
+                document.getElementById(ids[guessLetter]).style.borderColor = "rgb(211, 214, 218)"; // light grey
+            }
+            else if (ids.includes(event.key) && (guessLetter < (guessWordCount + 1) * numTiles)) { // Adding guessed letter
+                document.getElementById(ids[guessLetter]).innerText = event.key;
+                document.getElementById(ids[guessLetter]).style.borderColor = "rgb(129, 131, 132)"; // dark grey 
+                guessLetter++;
+            }
+            else if (event.key === "Enter" && (guessLetter % numTiles == 0) && guessLetter != 0) { // Word is guessed, move to next row
+                console.log("Word is guessed.")
+                guessWordCount++;
+                console.log(guessLetter)
+            }
+        });
+    })
 
 
     return (
         <div>
-            <div className="row">
-                {row1.map((tile) => <div className="tile">{tile}</div>)}
-            </div>
-            <div className="row">
-                {row2.map((tile) => <div className="tile">{tile}</div>)}
-            </div>
-            <div className="row">
-                {row2.map((tile) => <div className="tile">{tile}</div>)}
-            </div>
-            <div className="row">
-                {row3.map((tile) => <div className="tile">{tile}</div>)}
-            </div>
-            <div className="row">
-                {row4.map((tile) => <div className="tile">{tile}</div>)}
-            </div>
-            <div className="row">
-                {row5.map((tile) => <div className="tile">{tile}</div>)}
-            </div>
+            {[...Array(numRows)].map(() => 
+                <div className="row">
+                    {[...Array(numTiles)].map(() => <div className="tile" id={ids[idCount++]}></div>)}
+                </div>
+            )}
         </div>
-
     );
 }
 
 export { Board };
-
-/*
-<ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-*/
