ac70969ec1987baa70375d481aa182b6fb5744b3
Paolo Pedrigal <paolopedrigal@gmail.com>
2023-01-16 02:34:05 -0800

    Redirect to home when clicking on YOURDLE in NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import burgerMenu from "../images/burger.png";
 import questionMark from "../images/question-icon.jpg";
@@ -5,12 +6,14 @@ import settings from "../images/settings-gear-icon.webp";
 
 function NavBar() {
 
+    const navigate = useNavigate();
+
     return (
         <nav>
             <div className="more-info">
                 <img src={burgerMenu} alt="More Info" className="burger-menu"/>
             </div>
-            <h1 className="yourdle-title">YOURDLE</h1>
+            <h1 className="yourdle-title" onClick={() => navigate("/")}>YOURDLE</h1>
             <div className="options">
                 <img src={questionMark} alt="Help" className="question-mark"/>
                 <img src={settings} alt="Settings" className="settings"/>
