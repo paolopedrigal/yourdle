536352013ce83d0998a803cf4406f6758de66998
Paolo Pedrigal <paolopedrigal@gmail.com>
2023-02-23 00:01:22 -0800

    Created a help instructions for playing/viewing yourdle

--- client/src/routes/Yourdle.js
+++ client/src/routes/Yourdle.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar.js";
 import Game from "../components/Game.js";
+import raw from "../utils/help-yourdle.txt";
 
 function Yourdle() {
+
+    const [helpText, setHelpText] = useState("");
+
+    fetch(raw)
+        .then(r => r.text())
+        .then(text => {
+            setHelpText(text);
+        })    
+
     return (
         <div>
-            <NavBar moreInfo={true}/>
+            <NavBar 
+                moreInfo={true}
+                helpText={helpText}
+            />
             <Game />
         </div>
     );
