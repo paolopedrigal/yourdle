8bdb958815579deb4610977acf53960332caec84
Paolo Pedrigal <paolopedrigal@gmail.com>
2023-11-03 18:51:36 -0700

    Changing baseURL for axios

--- client/src/apis/Fetch.js
+++ client/src/apis/Fetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "api"
+    ? "http://yourdle.lol/api"
     : // "http://10.0.0.178:3001/api" ||
       "http://192.168.68.62:3001/api" || "http://localhost:3001/api"; // development mode (WSL | localhost)
 
